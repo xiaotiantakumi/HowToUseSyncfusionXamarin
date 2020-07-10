7b6f79ae8e364a9022261f3de72017e2f274a60b
Takumi Oda <xiaotiantakumi@gmail.com>
2020-07-10 23:31:26 +0900

    Sample1のブランチを作成

---
+++ HowToUseSyncfusionXamarin/GettingStarted1.xaml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.SfChart.XForms;
+using Xamarin.Forms;
+
+namespace HowToUseSyncfusionXamarin
+{
+
+    public partial class GettingStarted1 : ContentPage
+    {
+        public GettingStarted1()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+        }
+
+    }
+}
